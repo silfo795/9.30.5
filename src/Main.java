50c4841b070d3742d8b2b0dc52cd0a6e03576c8e
silfo <a143050@gmail.com>
2017-09-30 15:13:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner in=new Scanner(System.in);
+        int a,b,c;
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        System.out.printf("%d",(a+b)*c/2);
+
+
+    }
+}
